ceff5d81810d7ce16bc7efa760cd95067e85ec26
vt360 <enrico24680@gmail.com>
2025-11-15 16:29:16 +0100

    abilitato pinch to zoom sul mobile

---
+++ data.js
@@ -0,0 +1,163 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-foto-1-",
+      "name": "Foto 1 ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.31317550326645005,
+        "pitch": 0.028202043658692943,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8385812904639014,
+          "pitch": 0.0013747762940994335,
+          "rotation": 0,
+          "target": "2-foto-3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-foto-2-",
+      "name": "Foto 2 ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 2.6292789285868174,
+        "pitch": -0.013766137437652759,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.42399389024673617,
+          "pitch": -0.04927616676669899,
+          "rotation": 0,
+          "target": "3-foto-4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-foto-3-",
+      "name": "Foto 3 ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.383203408118315,
+        "pitch": 0.006350435867998883,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.14268093763085332,
+          "pitch": -0.04374387649096789,
+          "rotation": 0,
+          "target": "1-foto-2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-foto-4-",
+      "name": "Foto 4 ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.5919176542356404,
+        "pitch": 0.004151149591308467,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7439501424212027,
+          "pitch": -0.04009389625116455,
+          "rotation": 0,
+          "target": "0-foto-1-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "V._T._di_esempio_1",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
